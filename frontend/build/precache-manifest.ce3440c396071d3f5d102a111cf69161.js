ede566fca848dd9051c867e4cd3122a008258388
jordandev <rjaramilloby@gmail.com>
2020-10-26 14:16:30 -0500

    build agregado

---
+++ frontend/build/precache-manifest.ce3440c396071d3f5d102a111cf69161.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8715c2f8e8ad7c483e3d77df4713460d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded11509f8e65dbdd67b",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "74fc405f7c9ed3bd6743",
+    "url": "/static/css/main.2f3c27dc.chunk.css"
+  },
+  {
+    "revision": "ded11509f8e65dbdd67b",
+    "url": "/static/js/2.deace05f.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.deace05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fc405f7c9ed3bd6743",
+    "url": "/static/js/main.87b1e76d.chunk.js"
+  },
+  {
+    "revision": "5e60caa79ad49605ffbf",
+    "url": "/static/js/runtime-main.7d1d221e.js"
+  }
+]);
